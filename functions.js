79083dbede754cc726faaddd89eeed1fc0e74aa2
martynas3113 <martynas3113@users.noreply.github.com>
2020-11-14 11:51:11 +0200

    more-functions

--- functions.js
+++ functions.js
@@ -1,10 +1,5 @@
-function writeUsername (name) {
 
-    console.log('Hello from ' + name);
-}
-
-writeUsername('Karen')
-
+//Function that reverses given word
 
 function reverseWord (word){
     const input = word;
@@ -19,6 +14,7 @@ function reverseWord (word){
 
 reverseWord('methamfetamine')
 
+//Function finds how many times numbers from given interval can be divided by given number without remainder
 
 function division (interval1,interval2,divisionBy){
 
@@ -34,11 +30,12 @@ function division (interval1,interval2,divisionBy){
         }
         
     }
-    console.log(`Only ${result} numbers of interval from ${intervalas1} to ${intervalas2} can divide by ${daliklis} without remainder.`);
+    console.log(`Only ${result} numbers in interval from ${intervalas1} to ${intervalas2} can divide by ${daliklis} without remainder.`);
 }
 
 division(55,100,4)
 
+//Function finds highest number from array
 
 function highestNumber (arrayOfNumbers) {
     const numbers = arrayOfNumbers; /* Musu irasyta reiksme */
@@ -58,6 +55,7 @@ function highestNumber (arrayOfNumbers) {
 
 highestNumber([1,23,34,56,78])
 
+//Function sums ONLY positive integers from array
 
 function positiveSum(arr) {
     const array = arr;
@@ -70,13 +68,13 @@ function positiveSum(arr) {
         number = number+arr[i];
       }
     }
-    console.log(number);
+    console.log(`Sum of positive numbers is: ${number}`);
   }
 
   positiveSum([-1,-2,-3,4,5]);
 
 
-
+// Function finds only smallest number from array
 
   function smallestNumber (arrayOfNumbers) {
     const numbers = arrayOfNumbers; /* Musu irasyta reiksme */
@@ -94,17 +92,40 @@ function positiveSum(arr) {
 smallestNumber([23,45,-1,-45])
 
 
+//Function find Highest and Smallest number from array
 
-
-const numbers = [1,2,1,1,1,1,]
-let result = numbers[0];
-
-for (let i = 0;  i< numbers.length; i++) {
-    if (result !== numbers[i]) {
-        result = numbers[i];
-
-        
-    }
-    
+function SmallestAndHighestNumber(arrayOfNumbers) {
+const numbers = arrayOfNumbers;
+let result = arrayOfNumbers[0];
+let resultSmall = arrayOfNumbers[0];
+for (let i = 0; i < numbers.length; i++) {
+  if (result < numbers[i]){
+    result = numbers[i];
+  }
+  if(resultSmall > numbers[i]){
+    resultSmall = numbers[i]
+  }
 }
-console.log(result);
+console.log (`Highest number is ${result} , smallest number is ${resultSmall}`)
+}
+
+SmallestAndHighestNumber([1,2,3,-34,5,-100,120])
+
+//Function that finds name from array that has exactly 4 letters and considers it as your friend.
+
+function friendOrFoe(array){
+const list = array;
+let friend = [];
+
+for (let i = 0; i < list.length; i++) {
+    if (list[i].length ===4) {
+      friend.push(list[i])
+    } 
+  
+}
+console.log(`You can be sure that ${friend} is your friend(s)!`)
+
+}
+
+friendOrFoe(['Michael','John','Otto'])
+
